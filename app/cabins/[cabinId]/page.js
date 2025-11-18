bc56ae7d0170c0236d821369d127b3a45be1b863
M-Maaly <mo7amedm0stafa127@gmail.com>
2025-11-18 23:33:50 +0200

    build as a Static Site Generation SSG

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,20 +1,39 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
+export async function generateMetadata({ params }) {
+  const { name, description } = await getCabin(params.cabinId);
+  return {
+    title: `Cabin ${name}`,
+    description: description,
+  };
+}
 
- 
-export default async function Page({params}) {
-    const cabin = await getCabin(params.cabinId);
-    
-    const { id, name, maxCapacity, regularPrice, discount, image, description } =
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const paths = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+  
+  return paths;
+}
+
+export default async function Page({ params }) {
+  const cabin = await getCabin(params.cabinId);
+
+  const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
 
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
         <div className="relative scale-[1.15] -translate-x-3">
-          <Image src={image} fill className="object-cover" alt={`Cabin ${name}`} />
+          <Image
+            src={image}
+            fill
+            className="object-cover"
+            alt={`Cabin ${name}`}
+          />
         </div>
 
         <div>
