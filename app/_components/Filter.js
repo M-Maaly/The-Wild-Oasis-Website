b631d5fe11dc3319f0f69b6f7f16f6844b9b6cbf
M-Maaly <mo7amedm0stafa127@gmail.com>
2025-11-21 23:59:32 +0200

    append the interactivity between Server component and Client component

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -10,7 +10,7 @@ function Filter() {
       <button className="p-5 border-r border-primary-800 hover:bg-primary-700 w-full">
         4-7 guests
       </button>
-      <button className="p-5 hover:bg-primary-900 w-full">8+ guests</button>
+      <button className="p-5 hover:bg-primary-900 w-full"></button>
     </div>
   );
 }
