6b1ce764a66d455d35bf4638dad270ef90638613
M-Maaly <mo7amedm0stafa127@gmail.com>
2025-12-04 01:30:03 +0200

    user session

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -24,9 +24,9 @@ function ReservationForm({ cabin, user }) {
         </div>
       </div>
 
-      <p>
+      {/* <p>
         {String(range.from)} to {String(range.to)}
-      </p>
+      </p> */}
       <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
         <div className="space-y-2">
           <label htmlFor="numGuests">How many guests?</label>
